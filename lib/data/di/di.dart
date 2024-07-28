4c6da42af36c903458c225eb8f3fdd9241220b7c
mr.mojtaba <mr.mojtaba47@gmail.com>
2024-07-28 13:13:59 +0330

    fix : get news from server

--- lib/data/di/di.dart
+++ lib/data/di/di.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:news_core/data/network/news_api.dart';
+import 'package:news_core/data/repository/news_repository.dart';
 import 'package:news_core/data/source/remote_data_source.dart';
 
 final di = GetIt.instance;
@@ -7,5 +8,8 @@ final di = GetIt.instance;
 void setup({bool skipInit = false}) {
   //news core
   di.registerSingleton<NewsApi>(NewsApiImp());
-  di.registerSingleton<NetworkDataSourceImp>(NetworkDataSourceImp(di<NewsApi>()));
+  di.registerSingleton<NetworkDataSourceImp>(
+      NetworkDataSourceImp(di<NewsApi>()));
+  di.registerSingleton<NewsRepository>(
+      NewsRepository(remoteDataSource: di<NetworkDataSourceImp>()));
 }
--- packages/news_core/lib/data/source/remote_data_source.dart
+++ packages/news_core/lib/data/source/remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:news_core/data/model/model.dart';
 import 'package:news_core/data/network/news_api.dart';
 import 'package:news_core/data/source/data_source.dart';
@@ -9,16 +11,16 @@ class NetworkDataSourceImp extends DataSource {
 
   @override
   Future<List<Article>?> getNews({NewsSort? sort, int page = 1}) async {
-    await Future.wait([
+    return await Future.wait([
       newsApi.getTeslaNews(page: page, sort: sort),
       newsApi.getMicrosoftNews(page: page, sort: sort),
       newsApi.getAppleNews(page: page, sort: sort),
       newsApi.getGoogleNews(page: page, sort: sort),
     ]).then(
-          (value) {
+      (value) {
         List<Article>? result = [];
         int maxLength =
-        value.map((list) => list.length).reduce((a, b) => a > b ? a : b);
+            value.map((list) => list.length).reduce((a, b) => a > b ? a : b);
 
         for (int i = 0; i < maxLength; i++) {
           for (int j = 0; j < value.length; j++) {
@@ -27,9 +29,9 @@ class NetworkDataSourceImp extends DataSource {
             }
           }
         }
+        log('there is 2');
         return result.isNotEmpty ? result : null;
       },
     );
-    return null;
   }
 }
