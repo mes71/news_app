67a77b524e15441151199c1b1348acde18776496
mr.mojtaba <mr.mojtaba47@gmail.com>
2024-07-28 13:14:52 +0330

    feat : repository

--- lib/data/controller/root/root_controller.dart
+++ lib/data/controller/root/root_controller.dart
@@ -4,10 +4,9 @@ import 'dart:developer';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-
 import 'package:news_app/data/di/di.dart';
-import 'package:news_app/data/mapper/article_to_entity.dart';
 import 'package:news_app/data/routes/app_pages.dart';
+import 'package:news_core/data/repository/news_repository.dart';
 import 'package:news_core/news_core.dart';
 
 class RootController extends GetxController with StateMixin {
@@ -19,18 +18,16 @@ class RootController extends GetxController with StateMixin {
   RxList<Article> newsList = <Article>[].obs;
   Article? selectedArticle;
 
-  late NetworkDataSourceImp localRepository;
+  late NewsRepository newsRepository;
 
   @override
   void onInit() async {
     super.onInit();
-    localRepository = di<NetworkDataSourceImp>();
+    newsRepository = di<NewsRepository>();
     await initConnectivity();
 
     _connectivitySubscription =
         _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
-
-
   }
 
   Future<void> _updateConnectionStatus(List<ConnectivityResult> result) async {
@@ -49,56 +46,11 @@ class RootController extends GetxController with StateMixin {
     return _updateConnectionStatus(result);
   }
 
-/*  void changeCategory(NewsCategory category) {
-    selectedCategory.value = category;
-    getNews();
-  }*/
 
   void selectArticle(Article article) {
     selectedArticle = article;
     Get.toNamed(Routes.NEWS);
   }
-/*
-  Future<void> getNews() async {
-    change(null, status: RxStatus.loading());
-
-    if (connectionStatus.contains(ConnectivityResult.none)) {
-      // Load news from local storage if no internet connection
-      try {
-        List<ArticleEntity> cachedNews = localRepository.getAllNews();
-        if (cachedNews.isEmpty) {
-          change(null, status: RxStatus.empty());
-        } else {
-          newsList.clear();
-          newsList.addAll(cachedNews.map((e) => ArticleMapper.fromEntity(e)));
-          change(null, status: RxStatus.success());
-        }
-      } catch (e) {
-        log('Error loading cached news', error: e);
-        change(null, status: RxStatus.error('Error loading cached news'));
-      }
-    } else {
-      // Fetch news from the network
-      try {
-        NewsData data = await di<NewsRepository>()
-            .getNews(category: selectedCategory.value);
-        if (data.totalResults == 0) {
-          change(null, status: RxStatus.empty());
-        } else {
-          newsList.clear();
-          newsList.addAll(data.articles);
-          change(null, status: RxStatus.success());
-        }
-      } catch (e) {
-        log('Error fetching news', error: e);
-        change(null, status: RxStatus.error('Error fetching news'));
-      }
-    }
-  }
-
-  Future<void> cashNews(Article news) async {
-    await localRepository.addNews(ArticleMapper.toEntity(news));
-  }*/
 
   @override
   void dispose() {
@@ -106,7 +58,23 @@ class RootController extends GetxController with StateMixin {
     super.dispose();
   }
 
-  void getNews() async{
-    await localRepository.getNews();
+  void getNews() async {
+    change(null, status: RxStatus.loading());
+
+    try {
+      List<Article>? data = await newsRepository.getNews();
+
+      log(data.toString());
+      if (data?.isEmpty ?? false) {
+        change(null, status: RxStatus.empty());
+      } else {
+        newsList.clear();
+        newsList.addAll(data??[]);
+        change(null, status: RxStatus.success());
+      }
+    } catch (e) {
+      log('Error fetching news', error: e);
+      change(null, status: RxStatus.error('Error fetching news'));
+    }
   }
 }
---
+++ packages/news_core/lib/data/repository/news_repository.dart
@@ -0,0 +1,14 @@
+import 'dart:developer';
+
+import 'package:news_core/data/model/model.dart';
+import 'package:news_core/data/source/data_source.dart';
+
+class NewsRepository {
+  final DataSource remoteDataSource;
+
+  NewsRepository({required this.remoteDataSource});
+
+  Future<List<Article>?> getNews() async {
+    return await remoteDataSource.getNews();
+  }
+}
